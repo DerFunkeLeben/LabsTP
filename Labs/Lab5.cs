53f1a2adc4f56aede859712a71804da268bbdb1e
Egor Solonin <e.solonin@onpoint.ru>
2021-09-01 22:47:26 +0300

    v.1.0

--- Labs/Lab5.cs
+++ Labs/Lab5.cs
@@ -1,170 +1,201 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
 
 namespace Labs
 {
-    class Menu
-
+    public class Queue
     {
+        private string[] _array;
+        private int _head;
+        private int _tail;
 
-        public void ShowMenu(int n)
+        public Queue()
         {
-
-            Console.WriteLine("\n**МЕНЮ**\n");
-
-            Console.WriteLine($"|Выберите команду:\n| 1 - для создания нового полинома\n| 2 - для вывода имеющихся полиномов (Текущее кол-во созданых полиномов: {n}) \n| 3 - для дифференцирования выбранного полинома\n| 4 - для умножения двух выбранных полиномов\n| 5 - для завершения программы");
-
+            _array = new string[5];
         }
 
-        public void NewPoly(Polynomial[] X, int n)
+        public int Count { get; private set; }
 
+        public string[] ToArray
         {
-
-            Console.WriteLine("\n**CОЗДАНИЕ НОВОГО ПОЛИНОМА**\n");
-
-            Polynomial p = new Polynomial();
-
-            bool q = true;
-
-            int schet = 0;
-
-            while (q)
-
+            get
             {
+                var destinationArray = new string[Count];
+                Array.Copy(_array, _head, destinationArray, 0, Count);
+                return destinationArray;
+            }
+        }
 
-                Console.WriteLine($"|Текущее кол-во мономов в полиноме: {schet} \n|Ввод нового монома вида а*x^(b)*e^(c*x) \n");
+        public string Pop()
+        {
+            if (Count == 0)
+                return "**FIFO IS EMPTY**";
 
-                schet++;
+            var deletedItem = _array[_head];
+            _array[_head] = "";
+            _head = (_head + 1) % _array.Length;
+            Count--;
 
-                UInt32 coef, stx, ste, a;
+            return deletedItem;
+        }
 
-                Console.WriteLine("|Введите коэффициент монома: a = ");
+        public void Push(string item)
+        {
+            if (Count == _array.Length)
+                SetCapacity(_array.Length * 2);
 
-                coef = UInt32.Parse(Console.ReadLine());
+            _array[_tail] = item;
+            _tail = (_tail + 1) % _array.Length;
+            Count++;
+        }
 
-                Console.WriteLine("|Введите степень х: b = ");
-
-                stx = UInt32.Parse(Console.ReadLine());
-
-                Console.WriteLine("|Введите степень e: c = ");
-
-                ste = UInt32.Parse(Console.ReadLine());
-
-                p.AddMonom(new Monomial(coef, stx, ste));
-
-                Console.WriteLine("|Текущий вид многочлена: ");
-
-                Console.WriteLine(p.ToString());
-
-                Console.WriteLine("|Чтобы продолжить ввод мономов нажмите 1. Чтобы закончить создание полинома нажмите 2.\n|Ввод: ");
-
-                a = UInt32.Parse(Console.ReadLine());
-
-                if (a == 2)
-
-                    q = false;
-
+        private void SetCapacity(int capacity)
+        {
+            var destinationArray = new string[capacity];
+            if (Count > 0)
+            {
+                if (_head < _tail)
+                {
+                    Array.Copy(_array, _head, destinationArray, 0, Count);
+                }
+                else
+                {
+                    Array.Copy(_array, _head, destinationArray, 0, _array.Length - _head);
+                    Array.Copy(_array, 0, destinationArray, _array.Length - _head, _tail);
+                }
             }
 
-            X[n] = p;
+            _array = destinationArray;
 
-            Console.WriteLine($"|Получившийся полином сохранен под номером {n}\n");
-
-            X[n].ToString();
-
+            _head = 0;
+            _tail = Count == capacity ? 0 : Count;
         }
-
-        public void Spisok(Polynomial[] X, int n)
-
-        {
-
-            Console.WriteLine("\n**ВЫВОД ИМЕЮЩИХСЯ МНОГОЧЛЕНОВ**");
-
-            int i;
-
-            for (i = 1; i <= n; i++)
-
-            {
-
-                Console.WriteLine($"\n|Многочлен под номером {i}\n-> ");
-
-                Console.WriteLine(X[i].ToString());
-
-            }
-
-        }
-
-        public void multi(Polynomial[] X, int n)
-
-        {
-
-            Console.WriteLine("\n**УМНОЖЕНИЕ ДВУХ МНОГОЧЛЕНОВ**\n");
-
-            Console.WriteLine("|Выберите два многочлена которые хотите перемножить.");
-
-            for (int i = 1; i <= n; i++)
-
-            {
-
-                Console.WriteLine($"\n|Многочлен с номером {i}\n-> ");
-
-                Console.WriteLine(X[i].ToString());
-
-            }
-
-            Console.WriteLine("|Введите номера многочленов: ");
-
-            int a, b;
-
-            a = int.Parse(Console.ReadLine());
-
-            b = int.Parse(Console.ReadLine());
-
-            X[n + 1] = X[a] * X[b];
-
-            Console.WriteLine($"|Получившийся многочлен записан под номером {n + 1} и равен:\n-> ");
-
-            Console.WriteLine(X[n + 1].ToString());
-
-        }
-
-        public void Dif(Polynomial[] X, int n)
-
-        {
-
-            Console.WriteLine("\n**ДИФФЕРЕНЦИРОВАНИЕ ПОЛИНОМА**\n");
-
-            Console.WriteLine("|Выберите полином, который хотите продифференцировать.");
-
-            for (int i = 1; i <= n; i++)
-
-            {
-
-                Console.WriteLine($"\n|Полином с номером {i}\n-> ");
-
-                Console.WriteLine(X[i].ToString());
-
-            }
-
-            Console.WriteLine("|Введите номер полинома: ");
-
-            int a;
-
-            a = int.Parse(Console.ReadLine());
-
-            X[n + 1] = X[a].Differentiate();
-
-            Console.WriteLine($"|Получившийся полином \n-> ");
-
-            Console.WriteLine(X[n + 1].ToString());
-
-        }
-
     }
-    class Program
+
+    public class Menu
     {
-        static void Main(string[] args)
+        public void ShowMenu()
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("\n**MENU**\n");
+            Console.WriteLine("1. init new FIFO");
+            Console.WriteLine("2. print FIFO");
+            Console.WriteLine("3. push item");
+            Console.WriteLine("4. pop item");
+            Console.WriteLine("5. exit\n");
+
+            Console.ResetColor();
+        }
+
+        public void Init(Queue fifo)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n**CREATING NEW FIFO**\n");
+            Console.WriteLine("Input items. To finish - input keyword stop \n");
+
+            while (true)
+            {
+                var item = Console.ReadLine();
+                if (item == "stop")
+                    break;
+                fifo.Push(item);
+            }
+
+            Console.WriteLine("\nNew FIFO has been successfully created\n");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+        }
+
+        public void Print(Queue fifo)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n**PRINT FIFO**");
+            var size = fifo.Count;
+            if (size == 0)
+            {
+                Console.WriteLine("\n*---EMPTY---*");
+                Thread.Sleep(3000);
+                Console.ResetColor();
+                return;
+            }
+
+            var queue = fifo.ToArray;
+            Console.WriteLine("\n*---HEAD---*");
+
+            foreach (var item in queue)
+                Console.WriteLine(item);
+
+            Console.WriteLine("*---TAIL---*\n");
+
+            Thread.Sleep(3000);
+            Console.ResetColor();
+        }
+
+        public void Push(Queue fifo)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n**PUSH ITEM**\n");
+            Console.WriteLine("Input item you wanna push\n");
+
+            var item = Console.ReadLine();
+
+            fifo.Push(item);
+
+            Console.WriteLine("\n**SUCCESS**\n");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+        }
+
+        public void Pop(Queue fifo)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            var deletedItem = fifo.Pop();
+            Console.WriteLine(deletedItem == "**FIFO IS EMPTY**"
+                ? deletedItem
+                : $"\n**ITEM '{deletedItem}' HAS BEEN POPPED**\n");
+
+            Thread.Sleep(3000);
+            Console.ResetColor();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var fifo = new Queue();
+            var running = true;
+            while (running)
+            {
+                var menu = new Menu();
+                menu.ShowMenu();
+
+                var option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        menu.Init(fifo);
+                        break;
+                    case 2:
+                        menu.Print(fifo);
+                        break;
+                    case 3:
+                        menu.Push(fifo);
+                        break;
+                    case 4:
+                        menu.Pop(fifo);
+                        break;
+                    case 5:
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("ERROR - INCORRECT OPTION");
+                        break;
+                }
+            }
         }
     }
 }
